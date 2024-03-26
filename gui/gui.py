3aa90e8d98bd133687f6097f07db913950d6d249
AlexUrluescuWork <alexandre.urluescu@mtdtechnology.net>
2024-03-26 16:52:51 +0200

    implement the fundamental business logic

---
+++ gui/gui.py
@@ -0,0 +1,48 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton
+from utils import Utils
+from PyQt5.QtCore import QTimer
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.directory = 'Input Files'
+        self.outputFilePath = 'output.txt'
+        self.utils = Utils()
+
+        self.utils.getFilesFromDirectory(self.directory)
+
+        self.setWindowTitle("PyQt5 Template")
+        self.setGeometry(100, 100, 400, 200)
+
+        self.label = QLabel("Hello, PyQt5!", self)
+        self.label.setGeometry(50, 50, 300, 100)
+
+        self.button = QPushButton("Click Me", self)
+        self.button.setGeometry(150, 150, 100, 30)
+        self.button.clicked.connect(self.button_clicked)
+
+    def hideLoadingLabel(self):
+        self.label_loading.hide()
+
+    def button_clicked(self):
+        filesNames = self.utils.getFilesFromDirectory(self.directory)
+
+        self.label_loading = QLabel("Loading...", self)
+        self.label_loading.setGeometry(50, 50, 400, 200)
+        self.label_loading.show()
+
+        for file in filesNames:
+            barcode = self.utils.getBarcodeFromImage(file)
+            if(len(barcode) == 13):
+                self.utils.putNameAndBarecodeIntoTxtFile(file, self.outputFilePath, barcode)
+        
+        QTimer.singleShot(5000, self.hideLoadingLabel)
+
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
---
+++ gui/utils.py
@@ -0,0 +1,35 @@
+from typing import List
+import os
+import cv2
+
+class Utils():
+
+    def getFilesFromDirectory(self, directoryPath: str) -> List[str]:
+        file_names: List[str] = []
+        for filename in os.listdir(directoryPath):
+            if os.path.isfile(os.path.join(directoryPath, filename)):
+                file_names.append(filename)
+
+        print(file_names)
+        return file_names
+
+    
+    def getBarcodeFromImage(self, image: str):
+        img = cv2.imread(f'Input Files/{image}')
+
+        bd = cv2.barcode.BarcodeDetector()
+
+        retval, decoded_info, points = bd.detectAndDecode(img)
+
+        print(f"barcode: {retval} -> digits: {len(retval)}")
+        return retval
+    
+    
+    def putNameAndBarecodeIntoTxtFile(self, imageName: str, text_file_path: str, barcode):
+        try:
+            with open(text_file_path, 'a') as text_file:
+                text_file.write(f"{imageName} -> {barcode}\n")
+            
+        except Exception as e:
+            print("An error occurred:", str(e))
+
