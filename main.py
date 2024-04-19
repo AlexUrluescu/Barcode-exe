bf3700d6bd1c0ed93a9a33649956d038021af19d
AlexUrluescu <alexurluescu23@gmail.com>
2024-04-19 17:47:02 +0300

    delete print and entries ( optimization )

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 from utils import Utils
 
-print('INFO: The exe will search for an Input Files directory, please put your images in that directory')
-input("Press the enter key to proceed")
-
 directory = 'Input Files'
 outputFilePath = 'output.txt'
 utils = Utils()
@@ -13,19 +10,10 @@ running = True
 
 while running:
 
-    print("Scanning ...")
-    numberOfImages = 0
-
     for file in filesNames:
             if(file != ".DS_Store"):
                 barcode = utils.getBarcodeFromImage(directory, file)
                
                 utils.putNameAndBarcodeIntoTxtFile(file, outputFilePath, barcode)
-                numberOfImages = numberOfImages + 1
     
-    
-    print(f'Were scanned {numberOfImages} images')
-    exit = input("The barcode scan is done. Please press 0 to exit the program: ")
-    
-    if(exit == '0'):
-        running = False
+    running = False
